110e8b5ec202871e46a1d951c18bbc98fdf72273
James Traver <jtraverjr@gmail.com>
2015-07-13 22:11:43 -0500

    video complete

--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,19 @@ var app = app || {};
 // fetch the weather
 app.fetchWeather = function() {
 
-  $.getJSON("url", function(data) {
-    // things to do with the data!
+  $.getJSON("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=1111111111", function(data) {
+    app.renderWeather(data, "#body");
   });
+  return true;
 
 };
 // render the weather
-app.renderWeather = function(weatherData) {
+app.renderWeather = function(weatherData, selectorString) {
 
+  var template = weatherData.city.name + ", " + weatherData.city.country + " at (" + weatherData.city.coord.lon +"," + weatherData.city.coord.lat + ") and the current weather is..  " + weatherData.list[0].weather[0].description;
+
+  $(selectorString).html(template);
+  return true;
 
 };
 // when the document is ready
