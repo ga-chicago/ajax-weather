ea521e82ca7daae973cef09969f08abc1d44bea8
James Traver <jtraverjr@gmail.com>
2015-07-13 21:56:16 -0500

    skeleton

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,20 @@
+// namespace 'app'
+var app = app || {};
+// fetch the weather
+app.fetchWeather = function() {
+
+  $.getJSON("url", function(data) {
+    // things to do with the data!
+  });
+
+};
+// render the weather
+app.renderWeather = function(weatherData) {
+
+
+};
+// when the document is ready
 $(document).ready(function() {
-
-  console.log("All resources are loaded");
-
+  console.log("let's get ready to rumble!");
+  var weatherData = app.fetchWeather();
 });
